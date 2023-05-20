4f3eda20a7ad013b2e03673459cdb3b9993c67d4
unknown <bayuxbca@gmail.com>
2023-05-21 01:05:33 +0700

    make: footer as partials

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -692,17 +692,8 @@
     </section>
     </div>
 
-            <footer>
-                <div class="footer clearfix mb-0 text-muted">
-                    <div class="float-start">
-                        <p>2023 &copy; Larain Aja | Mazer Redev</p>
-                    </div>
-                    <div class="float-end">
-                        <p>Made with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                                href="https://byby.my.id">SplitSploit</a></p>
-                    </div>
-                </div>
-            </footer>
+        @include('partials.footer')
+
         </div>
     </div>
 @endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2023 &copy; Larain Aja | Mazer Redev</p>
+        </div>
+        <div class="float-end">
+            <p>Made with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                    href="https://byby.my.id">SplitSploit</a></p>
+        </div>
+    </div>
+</footer>
