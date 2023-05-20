9992e9e1284a0e1157980be97ca0c6983d70f4eb
unknown <bayuxbca@gmail.com>
2023-05-21 01:12:22 +0700

    make: menu as components

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index']);
+
+Route::view('components/alert', 'pages.components.alert')->name('components.alert');
